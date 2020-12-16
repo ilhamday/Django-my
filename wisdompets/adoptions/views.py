3e171a6a5a1c743beeac59d83901749e71ae9909
ilhamday <ilhamyudha00@gmail.com>
2020-12-16 14:35:53 +0700

    Edit views for home and pet_detail

--- wisdompets/adoptions/views.py
+++ wisdompets/adoptions/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 
-# Untuk percobaan pakai HttpResponse
-from django.http import HttpResponse
+from django.http import Http404
+from .models import Pet # import Pet model from models.py
 
 def home(request):
-    return HttpResponse('<p>home view</p>')
+    pets = Pet.objects.all() # <- query ORM 
+    return render(request, 'home.html', {
+        'pets': pets, # <- render function takes a dictionary with the data we want to make available inside of template
+    })
 
 def pet_detail(request, pet_id):
-    return HttpResponse(f'<p>pet_detail view with id {pet_id} </p>')
+    try:
+        pet = Pet.objects.get(id=pet_id)
+    except Pet.DoesNotExist:
+        raise Http404('Pet not found')
+
+    return render(request, 'pet_detail.html', {
+        'pet': pet,
+    })
